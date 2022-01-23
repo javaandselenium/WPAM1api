fd09326ed29b7354f3f8b73cd544d812fddf6210
QSP <QSP@QSP>
2022-01-23 09:47:43 +0530

    new files

--- src/test/java/com/Ty/Github/TestScripts/Demo2.java
+++ src/test/java/com/Ty/Github/TestScripts/Demo2.java
@@ -3,10 +3,10 @@ package com.Ty.Github.TestScripts;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class Demo {
+public class Demo2 {
 	@Test
-	public void tc1() {
-		Reporter.log("Hello api",true);
+	public void tc2() {
+		Reporter.log("Hello Postaman",true);
 	}
 
 }
